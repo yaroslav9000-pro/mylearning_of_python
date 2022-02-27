b69e326cc3ed6121371f4ae9d0d03bae1fb0f04f
YaroslavYarkevi <>
2022-02-27 21:49:52 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+print('Greetings, welcome to bandname creater!!!')
+bcity = input('What city you grew up in?\n')
+pet_name = input('What is your pet name?\n'
+)
+print(bcity + " " + pet_name)                 
