893d6323284cdc48a0743296483cc1b0b64530eb
Yaroslav Yarkevich <100991196+yaroslav9000-pro@users.noreply.github.com>
2022-03-05 07:50:31 +0000

    tralalil

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-print('Greetings, welcome to bandname creater!!!')
-bcity = input('What city you grew up in?\n')
-pet_name = input('What is your pet name?\n'
-)
-print(bcity + " " + pet_name)                 
+print('welcome to tip calculator!!!')
+bill = input('How much is your bill?\n')
+people_num = int(input('Between how many people you would like to share the bill.\n'))
+tip = int(input('What percentage tip you would like to give, 10/12/15?\n')) / 100
+total_sum = float(bill) * (1 + tip)
+each_person_pay = round(total_sum / people_num)
+print('Each person should pay: {}'.format(each_person_pay))
