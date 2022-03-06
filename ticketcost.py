dc7be54fd817c9059eb52057219fb397c06c0819
yaroslav9000-pro <100991196+yaroslav9000-pro@users.noreply.github.com>
2022-03-06 05:49:52 +0200

    Add files via upload

---
+++ ticketcost.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+print("Welcome to rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+    print("You can ride the rollercoaster.")
+    age = int(input("What is your age? "))
+    if age < 12:
+        print("Please pay $5")
+    elif 12 <= age < 18:
+        print("Please pay $7.")
+    else:
+        print("Please pay $12.")    
+else:
+    print("You should grow taller to ride.")
+
+
+
